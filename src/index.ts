747cf708b78fa9696a1e8cfda3448af6b91486d2
9hwng <39527533+9hwng@users.noreply.github.com>
2025-06-24 13:09:07 +0900

    Remove font utilities from the project by deleting the fonts.ts file and updating index.ts to exclude font-related exports, streamlining the codebase.

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,5 @@ export { Button } from "./components/Button/Button";
 export type { ButtonProps } from "./components/Button/Button";
 export { cn } from "./utils/cn";
 
-// Export font utilities
-export {
-  fontCSSVariables,
-  FONT_FAMILIES,
-  FONT_WEIGHTS,
-  getFontFamily,
-  getFontWeight,
-  fontClasses,
-  typographyClasses,
-} from "./utils/fonts";
-
 // Export colors for use in other projects
 export { colors } from "./styles/colors";
